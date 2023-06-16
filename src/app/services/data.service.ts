adeed8190683f3e5c684245068f41960f5b8d5ff
Steven Garcia <Garciaintech@gmail.com>
2023-06-15 22:17:18 -0400

    add service to get user data

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
-
+import { Users } from '../types/users';
+import { HttpClient } from '@angular/common/http'
+import {
+  Observable,
+  map,
+  pipe, switchMap
+} from 'rxjs'
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  getExistingUsersData$(): Observable<Users[]> {
+    return this._http.get<Users[]>('localhost:4200/getUsers').pipe(
+      map((data: Users[]) => {
+        return data
+      })
+    );
+  }
 }
---
+++ src/app/types/users.ts
@@ -0,0 +1,15 @@
+export interface Users {
+    firstName: string;
+    lastName: string;
+    userId: number;
+    taskList: Task[];
+}
+
+export interface Task {
+    taskId:number;
+    dateCreated: Date;
+    title: string;
+    description: string;
+    complete: boolean;
+    dateCompleted: Date;
+}
